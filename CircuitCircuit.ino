7a6e67d80ae2bd809cb9ffe3c7237eeffa6640e6
PCswanson <kris.swanson@pinecrest.edu>
2017-04-23 19:34:22 -0400

    Version 1.1
    Changed keys, added light codes, added temp and light level functions

--- CircuitCircuit.ino
+++ CircuitCircuit.ino
@@ -1,7 +1,21 @@
 /*
- * Circuit Circuit - Makey Makey simulator for Adafruit Circuit Playground
- * Version 1.0 by Kris Swanson, 9/2016
+ * Circuit Circuit - Makey Makey - like functions for Adafruit Circuit Playground
+ * Version 1.1 by Kris Swanson, 4/2017
+ * Changelog:
+ *  Added lights code on contact, changed keyboard keys
+ *  Added on-board button functions:
+ *      -right button gives Temp F (uncomment line for Temp C)
+ *      -left button gives light level (0-100)
+ *      -both are formatted for easy use with Scratch Question block. Example:
+ *            Pressing right button sends "t##" then carriage return. ## is temp
+ *            Pressing left button sends "l###" then carrage return ### is light level
  * Thanks to Tony DiCola and Adafruit for megademo code which this is based on.
+ * Usage notes:
+ *                - If you will only be touching one contact at at time,
+ *                you don't need to be grounded, but if you are going to be 
+ *                touching more than one contact at a time you do need to 
+ *                be grounded.
+ 
  */
 
 
@@ -19,9 +33,14 @@
 #define LEFT_KEY 6  // Set pin for Left Arrow Key
 #define RIGHT_KEY 12  // Set pin for Right Arrow Key
 #define SPACE_KEY 1  // Set pin for Space Bar
-#define SHIFT_KEY 0  // Set pin for Command Key
-#define CTRL_KEY 2  // Set pin for ALT Key
-#define MOUSE_CLICK 3 // Set pin for Left Mouse Click
+#define C_KEY 0  // Set pin for c Key
+#define B_KEY 2  // Set pin for b Key
+#define A_KEY 3 // Set pin for a key
+
+int temp;
+int light;
+bool leftButtonPressed;
+bool rightButtonPressed;
 
 void setup() {
 
@@ -43,19 +62,23 @@ void loop() {
     Watchdog.sleep(1000);
   }
 
- 
+  
    //// Clear all the neopixels.
 
+  CircuitPlayground.clearPixels();
    
-    for (int i=0; i<10; ++i) {
+ /*   for (int i=0; i<10; ++i) {
       CircuitPlayground.strip.setPixelColor(i, 0);
     }
-    
+ */   
     // Check if any of the cap touch inputs are pressed and turn on those pixels.
-    // Also play a tone if in tone playback mode.
+    
     
     if (CircuitPlayground.readCap(UP_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(3, CircuitPlayground.colorWheel(0));
+      CircuitPlayground.strip.setPixelColor(0, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(1, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(8, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(9, 0xFFFFFF);
       Keyboard.press(0xda);
     }
     else {
@@ -64,7 +87,10 @@ void loop() {
     
 
     if (CircuitPlayground.readCap(DOWN_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(4, CircuitPlayground.colorWheel(25));
+      CircuitPlayground.strip.setPixelColor(3, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(4, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(5, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(6, 0xFFFFFF);
       Keyboard.press(0xd9);
     }
     else {
@@ -72,7 +98,11 @@ void loop() {
     }
 
   if (CircuitPlayground.readCap(RIGHT_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(1, CircuitPlayground.colorWheel(50));
+      CircuitPlayground.strip.setPixelColor(5, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(6, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(7, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(8, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(9, 0xFFFFFF);
       Keyboard.press(0xd7);
     }
     else {
@@ -80,7 +110,11 @@ void loop() {
     }
 
   if (CircuitPlayground.readCap(LEFT_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(0, CircuitPlayground.colorWheel(75));
+      CircuitPlayground.strip.setPixelColor(0, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(1, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(2, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(3, 0xFFFFFF);
+      CircuitPlayground.strip.setPixelColor(4, 0xFFFFFF);
       Keyboard.press(0xd8);
     }
     else {
@@ -88,38 +122,72 @@ void loop() {
     }
 
   if (CircuitPlayground.readCap(SPACE_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(9, CircuitPlayground.colorWheel(100));
+      for(int i=0; i<10; ++i){
+        CircuitPlayground.strip.setPixelColor(i, 0xFFFFFF);
+      }
       Keyboard.press(' ');
     }
     else {
       Keyboard.release(' ');
     }
 
-  if (CircuitPlayground.readCap(SHIFT_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(8, CircuitPlayground.colorWheel(125));
-      Keyboard.press(0x81);
+  if (CircuitPlayground.readCap(A_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
+      for(int i=0; i<10; ++i){
+        CircuitPlayground.strip.setPixelColor(i, 0xFF0000);
+      }
+      Keyboard.press('a');
     }
     else {
-      Keyboard.release(0x81);
+      Keyboard.release('a');
     }  
 
-  if (CircuitPlayground.readCap(CTRL_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(6, CircuitPlayground.colorWheel(150));
-      Keyboard.press(0x80);
+  if (CircuitPlayground.readCap(B_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
+      for(int i=0; i<10; ++i){
+        CircuitPlayground.strip.setPixelColor(i, 0x00FF00);
+      }
+      Keyboard.press('b');
     }
     else {
-      Keyboard.release(0x80);
+      Keyboard.release('b');
     }  
 
-  if (CircuitPlayground.readCap(MOUSE_CLICK, CAP_SAMPLES) >= CAP_THRESHOLD) {
-      CircuitPlayground.strip.setPixelColor(5, CircuitPlayground.colorWheel(175));
-      Mouse.click();
+  if (CircuitPlayground.readCap(C_KEY, CAP_SAMPLES) >= CAP_THRESHOLD) {
+      for(int i=0; i<10; ++i){
+        CircuitPlayground.strip.setPixelColor(i, 0x0000FF);
+      }
+      Keyboard.press('c');
     }
     else {
-      Mouse.release();
+      Keyboard.release('c');
     }  
 
 
   // Light up the pixels.
-    CircuitPlayground.strip.show();
-  }   
+  CircuitPlayground.strip.show();
+
+  temp = int(CircuitPlayground.temperatureF());
+  // Uncomment the next line to switch to celsius
+  //temp = int(CircuitPlayground.temperature());
+
+  light = int(CircuitPlayground.lightSensor());
+
+  leftButtonPressed = CircuitPlayground.leftButton();
+  rightButtonPressed = CircuitPlayground.rightButton();
+
+//if left button pressed, send tempature data
+  if(leftButtonPressed) {
+    Keyboard.print("t");
+    delay(400);
+    Keyboard.println(temp);
+    Keyboard.write(0xB0);
+  }
+
+//if right button pressed send light data (0-1024)
+  if(rightButtonPressed) {
+    Keyboard.print("l");
+    delay(400);
+    Keyboard.println(light);
+    Keyboard.write(0xB0);
+  }
+    
+  }
